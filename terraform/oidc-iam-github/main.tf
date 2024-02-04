1c16c57937a3fb23e2e37006e897318b2be6001d
shubham <om08021998@gmail.com>
2024-02-04 21:01:26 +0530

    update actions

--- terraform/oidc-iam-github/main.tf
+++ terraform/oidc-iam-github/main.tf
@@ -55,6 +55,7 @@ data "aws_iam_policy_document" "github_actions_cloudfront_site" {
 
     actions = [
       "s3:ListBucket",
+      "s3:GetObject",
       "s3:GetObjectAcl",
       "s3:GetObjectTagging",
       "s3:ListBucket",
@@ -62,6 +63,11 @@ data "aws_iam_policy_document" "github_actions_cloudfront_site" {
       "s3:PutObject",
       "s3:PutObjectAcl",
       "s3:PutObjectTagging",
+      "s3:DeleteObject",
+      "s3:GetObjectVersion",
+      "s3:ListBucketVersions",
+      "s3:PutObjectVersionAcl",
+      "s3:PutObjectVersionTagging"
     ]
 
     resources = [
